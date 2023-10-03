1c504d6d30ef2251cde74c5eb0976875b50f3068
MIMIMINISAIA <emillybeatrizamoroso@Gmail.com>
2023-10-03 15:39:05 -0300

    atualizando

---
+++ database/migrations/2023_10_03_165724_agenda_migration.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('profissional_id')->nullable(false);
+            $table->bigInteger('cliente_id')->nullable(false);
+            $table->bigInteger('servico_id')->  nullable(false);
+            $table->date('data_hora')->nullable(false);
+            $table->string('tipo_pagamento', 20)->nullable(false);
+            $table->decimal('valor')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profissionals');
+    }
+};
